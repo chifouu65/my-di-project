68d80c5e4068e15e1f4749ed494c2820f242559b
noahl <noah.lhote56@gmail.com>
2024-06-02 23:41:31 +0200

    nit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from "./components/footer/footer.component";
+import {HeaderComponent} from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet,  FooterComponent, HeaderComponent],
+  template: `
+    <app-header/>
+    <main id="main" class="min-h-screen">
+      <router-outlet/>
+    </main>
+    <app-footer/>
+  `,
 })
 export class AppComponent {
   title = 'my-digital-project';
